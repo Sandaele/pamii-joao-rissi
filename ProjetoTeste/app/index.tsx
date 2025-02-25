9d35db6ebca5fb0de0c1b3b6731a203e9b4c3dfa
Sandaele <sandalia207@gmail.com>
2025-02-25 17:56:56 -0300

    Adiciona Projeto :)

---
+++ ProjetoTeste/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { Image } from 'expo-image';
+
+const PlaceholderImage = require("@/assets/images/pao-com-chauchicha.png")
+
+export default function Index() {
+  return (
+    <View>-9S
+    </View>
+  );
+}
