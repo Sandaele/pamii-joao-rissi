58bc46e4af5d0f1964eef47b5e4bdcd500aa6a33
sandaele <sandalia207@gmail.com>
2025-03-11 18:08:11 -0300

    segunda aula

--- ProjetoTeste/app/index.tsx
+++ ProjetoTeste/app/index.tsx
@@ -1,11 +1,48 @@
 import { Text, View } from "react-native";
-import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require("@/assets/images/pao-com-chauchicha.png")
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
-    <View>-9S
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Enviar Imagem" onPress={pickImageAsync} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
