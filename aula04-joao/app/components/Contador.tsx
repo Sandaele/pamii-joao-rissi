742a432268561137fa14d99bd244b465230be1e1
sandaele <sandalia207@gmail.com>
2025-05-06 17:54:02 -0300

    finalizacao dos contador

--- aula04-joao/app/components/Contador.tsx
+++ aula04-joao/app/components/Contador.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Button, View, StyleSheet } from
+import { Text, Button, View, styles } from
 'react-native';
 
 export default class Cotador extends React.Component {
@@ -22,17 +22,35 @@ export default class Cotador extends React.Component {
     render() {
         return (
             <View>
-                <View style={StyleSheet.texto}>
+                <View style={styles.texto}>
                     <Text>{this.state.contador}</Text>
                 </View>
-                <View style={StyleSheet.botoes}>
+                <View style={styles.botoes}>
                     <Button
-                        style={StyleSheet.botao1}
+                        style={styles.botao1}
                         title="Aumenta"
                         onPress={this.Incrementar.bind(this)}
-                    >
+                    />
+                    <Button
+                        style={styles.botao2}
+                        onPress={this.Decrementar.bind(this)}
+                        title="Diminui"
+                    />
                 </View>
             </View>
-        )
+        );
     }
 }
+const styles = StyleSheet.create({
+    botoes: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        top: 220,
+        height: 40,
+    },
+    texto: {
+        alignitems: 'center',
+        top: 200,
+    }
+})
