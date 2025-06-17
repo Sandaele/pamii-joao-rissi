6fe546d905fcf8c1616645f0e9133f93abf2e6d7
sandaele <sandalia207@gmail.com>
2025-06-17 17:59:07 -0300

    atualiza o contador

--- aula04-joao/app/components/Contador.tsx
+++ aula04-joao/app/components/Contador.tsx
@@ -1,55 +1,56 @@
 import React from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
+import {Text, Button, View, StyleSheet} from 'react-native';
 
-export default class Cotador extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contador: 0,
-        };
-    }
-    Decrementar() {
-        this.setState({
-            contador: this.state.contador - 1
-        });
-    }
-    Incrementar() {
-        this.setState({
-            contador: this.state.contador + 1
-        });
-    }
-    render() {
-        return (
-            <View>
-                <View style={styles.texto}>
-                    <Text>{this.state.contador}</Text>
-                </View>
-                <View style={styles.botoes}>
-                    <Button
-                        style={styles.botao1}
-                        title="Aumenta"
-                        onPress={this.Incrementar.bind(this)}
-                    />
-                    <Button
-                        style={styles.botao2}
-                        onPress={this.Decrementar.bind(this)}
-                        title="Diminui"
-                    />
-                </View>
-            </View>
-        );
-    }
+export default class Contador extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      contador: 0,
+    };
+  }
+  Decrementar(){
+    this.setState({
+      contador: this.state.contador - 1
+  });
+  }
+  Incrementar() {
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  }
+  render(){
+    return (
+      <View>
+        <View style={styles.texto}>
+          <Text>{this.state.contador}</Text>
+        </View>
+        <View style={styles.botoes}>
+        <Button
+          style={styles.botao1}
+          title="Aumenta"
+          onPress={this.Incrementar.bind(this)}
+        />
+        <Button
+          style={styles.botao2}
+          title="Diminui"
+          onPress={this.Decrementar.bind(this)}
+        />
+        </View>
+      </View>
+    )
+  }
 }
+
 const styles = StyleSheet.create({
-    botoes: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignSelf: 'center',
-        top: 220,
-        height: 40,
-    },
-    texto: {
-        alignitems: 'center',
-        top: 200,
-    }
-})
+  botoes: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    top: 220,
+    height: 40,
+  },
+  texto: {
+    alignItems: 'center',
+    top: 200,
+  },
+});
